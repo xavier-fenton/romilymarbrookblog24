2815439746208eac5714e747079e71874b0cd1ad
Xavier Fenton <xavierfenton99@gmail.com>
2023-12-30 12:25:17 +1300

    initial commit. setup static content

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../styles/footer.css'
+export default function Footer() {
+  return (
+    <>
+      <div className="footer-main-wrapper">
+        <div className="footer-border"></div>
+        <div className="footer-content-wrapper">
+          <div className="footer-title">
+            2023 © <span className="contact-underline">contact</span>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/header.css'
+export default function Header() {
+  return (
+    <>
+      <div className="header-main-wrapper">
+        <div className="header-title">romily</div>
+        <div className="header-about">about</div>
+      </div>
+      <div className="header-border"></div>
+    </>
+  )
+}
